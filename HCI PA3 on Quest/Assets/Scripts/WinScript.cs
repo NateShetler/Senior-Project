9233412c996bbf873fec14f40583f29a3952282e
NateShetler <46767144+NateShetler@users.noreply.github.com>
2020-08-29 20:47:24 -0400

    add HCI PA3 ported over to Quest

---
+++ HCI PA3 on Quest/Assets/Scripts/WinScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinScript : MonoBehaviour
+{
+    public Text winMessage;
+    public Text instructions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        winMessage.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // This will detect when the ball goes into the hole
+    void OnCollisionEnter(Collision holeCol)
+    {
+        if (holeCol.gameObject.name == "Plane Under Hole")
+        {
+            // Tell the player they have won
+            winMessage.text = "You have won!";
+
+            // Get rid of the instructions text
+            instructions.text = "";
+        }
+    }
+}
---
+++ HCI PA3 on Quest/ReadMe.txt.txt
@@ -0,0 +1,7 @@
+Nathaniel Shetler
+PA3
+
+Included is the PA3 folder which is a Unity3D project folder that includes all of the 
+necessary files that created the program. If somebody wants to build the program they
+can open up the unity project and build it. The source code for the C# scripts used can be
+found insid the PA3 folder with the file path PA3/Assets/Scripts. 
